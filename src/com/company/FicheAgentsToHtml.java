e36433e9b8d1a44686245f70f782fcaf5c8ec4c8
cbisso11 <clement.bisson@epsi.fr>
2021-05-12 23:04:00 +0200

    msprJavaV3

--- src/com/company/FicheAgentsToHtml.java
+++ src/com/company/FicheAgentsToHtml.java
@@ -11,29 +11,65 @@ public class FicheAgentsToHtml {
         html.append( "<head>\n" );
         html.append( "<meta charset='utf-8'>\n" );
         html.append( "<title>Fiche Agent</title>\n" );
-        html.append( "<link rel=\"stylesheet\" href=\"../stylesheet.css\">" );
-        html.append( "</head>\n\n" );
-        html.append( "<body>\n" );
+        html.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+        html.append("<link href=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" rel=\"stylesheet\">");
+        html.append("<link rel=\"stylesheet\" href=\"../stylesheet.css\">");
+        html.append("</head>");
+        html.append("<header>");
+        html.append("    <div id=\"header\">");
+        html.append("        <h4 id=\"titre\">FICHE AGENT</h4>");
+        html.append("        <img src=\"IMAGES/logo.png\" alt=\"\" id=\"logo\" width=\"171\" /> ");
+        html.append("        <a href=\"../index.html\"><img id=\"accueil\" width=\"80\" src=\"../images/accueil.png\"/></a>");
+        html.append("    </div>");
+        html.append("</header>");
+        html.append("<body>\n");
+        html.append("<div class=\"container\">");
+        html.append("<div id=\"content\" class=\"content p-0\">");
+        html.append("    <div class=\"profile-header\">");
+        html.append("        <div class=\"profile-header-cover\"></div>");
+        html.append("        <div class=\"profile-header-content\">");
+        html.append("            <div class=\"profile-header-img\">");
         String ID = agents.getPrenom().charAt(0) + agents.getNom();
-        html.append("<h1> "+
-                 agents.getNom()  +" " +  agents.getPrenom() +"\n" +
-                "<img class=\"photo\" src=\"../images/"+ ID.toLowerCase() +".jpg\" />\n" +
-                "\n" +
-                "</h1>\n");
-        html.append("<div id=\"materiel\">\n");
-        html.append("<table id=\"tablemateriel\">\n");
-        html.append("<tr>\n" +
-                "\t    <th width=\"250\"></td>\n" +
-                "\t    <th width=\"50\"></td>\n" +
-                "\t  </tr>\n");
+        html.append("                <img src=\"../images/"+ ID.toLowerCase() +".jpg\" alt=\"\" />");
+        html.append("            </div>");
+        html.append("            <div class=\"profile-header-info\">");
+        html.append("                <h4 class=\"m-t-sm\">"+agents.getNom()  +" "+  agents.getPrenom()+"</h4>");
+        html.append("            </div>");
+        html.append("        </div>");
+        html.append("    </div>");
+        html.append("    <div class=\"profile-container\">");
+        html.append("        <div class=\"row row-space-20\">");
+        html.append("            <div class=\"col-md-8\" id=\"tableau\">");
+        html.append("                <div class=\"tab-content p-0\">");
+        html.append("                    <div class=\"tab-pane active show\" id=\"profile-about\">");
+        html.append("                        <table class=\"table table-profile\">");
+        html.append("                            <thead>");
+        html.append("                                <tr>");
+        html.append("                                    <th colspan=\"2\">Matériels</th>");
+        html.append("                                </tr>");
+        html.append("                            </thead>");
+        html.append("                            <tbody>");
         for ( String objet : agents.getObjet()) {
-            html.append( "<tr> <td> "+ objet + "</td> <td><img id=\"case\" src=\"../images/image2.png\"/> </td> </tr>\n" );
+            html.append( "                           <tr>\n" +
+                    "                                    <td ><img id=\"case\" src=\"../images/case.png\"/></td>\n" +
+                    "                                    <td>"+ objet +"</td>\n" +
+                    "                                </tr>" );
         }
-        html.append( "</table>\n" );
-        html.append( "</div>\n" );
-        html.append( "</body>\n\n" );
-        html.append( "</html>" );
-
+        html.append("                            </tbody>");
+        html.append("                        </table>");
+        html.append("                    </div>");
+        html.append("                </div>");
+        html.append("            </div>");
+        html.append("        </div>");
+        html.append("    </div>");
+        html.append("</div>");
+        html.append("</div>");
+        html.append("<script src=\"http://code.jquery.com/jquery-1.10.2.min.js\"></script>");
+        html.append("<script src=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\"></script>");
+        html.append("<script type=\"text/javascript\">");
+        html.append("</script>");
+        html.append("</body>");
+        html.append("</html>");
         return html.toString();
     }
 }
--- src/com/company/FicheCSS.java
+++ src/com/company/FicheCSS.java
@@ -4,52 +4,190 @@ public class FicheCSS {
     public String render() {
 
         StringBuilder html = new StringBuilder();
-        html.append( "@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap%27');\n" +
-                "@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');\n" +
+        html.append( "/* import fonts */\n" +
+                "@font-face {\n" +
+                "    font-family: \"RobotoMedium\";\n" +
+                "    src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap%27');\n" +
+                "}\n" +
                 "\n" +
+                "@font-face {\n" +
+                "    font-family: \"RobotoLight\";\n" +
+                "    src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');\n" +
+                "}\n" +
                 "\n" +
+                "body{\n" +
+                "    background: #eaeaea;\n" +
+                "    margin-top:20px;\n" +
+                "    font-family: 'RobotoLight', sans-serif;\n" +
+                "}\n" +
                 "\n" +
+                "header{\n" +
+                "    width: 100%;\n" +
+                "    margin-bottom: 2%;\n" +
+                "    border-bottom: 2px black solid;\n" +
+                "    height: 100px;\n" +
+                "}\n" +
+                "\n" +
+                "/* image check */\n" +
                 "#case {\n" +
-                "\twidth:15px;\n" +
-                "}\n" +
-                "img.photo {\n" +
-                "\n" +
-                "max-width: 15%;\n" +
-                "float: right;\n" +
-                "margin-right: 40px;\n" +
-                "\n" +
+                "    width:15px;\n" +
                 "}\n" +
                 "\n" +
-                "button {\n" +
-                "\n" +
-                "font-size : 26px;\t\n" +
-                "font-weight: normal;\n" +
-                "margin-left: 40px;\n" +
+                "/* image logo entreprise */\n" +
+                "#logo {\n" +
+                "    float: left;\n" +
+                "    margin-top: -3%;\n" +
                 "}\n" +
                 "\n" +
-                "p{\n" +
+                "/* titre fiche agent */\n" +
+                "#titre{\n" +
+                "    text-align: center;\n" +
+                "    font-family: 'RobotoLight', sans-serif;\n" +
+                "}\n" +
                 "\n" +
-                " font-weight: lighter;\n" +
-                "font-family: 'Roboto', sans-serif;\n" +
-                " text-align: center;\n" +
-                " font-size : 20px;\n" +
-                " height: 600px;\n" +
+                "/* image home */\n" +
+                "#accueil {\n" +
+                "    float: right;\n" +
+                "    padding-top: 1%;\n" +
+                "    padding-right: 1%;\n" +
+                "    margin-top: -3%; \n" +
+                "}\n" +
+                "\n" +
+                "/* nom */\n" +
+                "h4 {\n" +
+                "font-size : 32px;\n" +
+                "font-family: 'RobotoLight', sans-serif;\n" +
+                "}\n" +
+                "\n" +
+                "/* matériels */\n" +
+                "table {\n" +
+                "    margin: auto;\n" +
+                "    color: #659224;\n" +
                 "}\n" +
                 "\n" +
                 "#materiel{\n" +
-                "\tmargin-top: 23%;\n" +
-                "\ttext-align: right;\n" +
-                "\tdisplay: block;\n" +
+                "    margin-top: 20%;\n" +
+                "    text-align: left;\n" +
+                "    display: block;\n" +
+                "    font-size : 26px;    \n" +
                 "}\n" +
                 "\n" +
-                "table{\n" +
-                "\tmargin: auto;\n" +
+                "#tableau {\n" +
+                "     margin : auto;\n" +
                 "}\n" +
                 "\n" +
-                "body {\n" +
-                " text-align: bottom;\n" +
-                " font-family: 'Roboto', sans-serif;\n" +
-                " font-weight: bold;\n" +
+                ".container{\n" +
+                "    border : 2px black solid;\n" +
+                "    padding: 0;\n" +
+                "}\n" +
+                "\n" +
+                ".table.table-profile th {\n" +
+                "    border: none;\n" +
+                "    color: #000;\n" +
+                "    padding-bottom: 0.3125rem;\n" +
+                "    padding-top: 0;\n" +
+                "\n" +
+                "}\n" +
+                ".table.table-profile td {\n" +
+                "    border-color: #c8c7cc;\n" +
+                "    color: #659224;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header {\n" +
+                "    position: relative;\n" +
+                "    overflow: hidden;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header .profile-header-cover {\n" +
+                "    background-color: #379EC1;\n" +
+                "    position: absolute;\n" +
+                "    left: 0;\n" +
+                "    right: 0;\n" +
+                "    top: 0;\n" +
+                "    bottom: 0;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header .profile-header-content,\n" +
+                ".profile-header-img, .profile-header-img,\n" +
+                "body .fc-icon { \n" +
+                "    position: relative;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header .profile-header-content {\n" +
+                "    color: #fff;\n" +
+                "    padding: 1.25rem;\n" +
+                "    height: 200px;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header-img {\n" +
+                "    float: right;\n" +
+                "    width: 7.5rem;\n" +
+                "    height: 7.5rem;\n" +
+                "    margin: 0 1.25rem -1.25rem 0;\n" +
+                "    padding: 0.1875rem;\n" +
+                "    border-radius: 0.25rem;\n" +
+                "    background: #fff;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header-info h4 {\n" +
+                "    font-weight: 500;\n" +
+                "    margin-bottom: 0.3125rem;\n" +
+                "}\n" +
+                "\n" +
+                ".profile-container {\n" +
+                "    padding: 1.5625rem;\n" +
+                "}\n" +
+                "\n" +
+                "@media (max-width: 967px) {\n" +
+                "    .profile-header-img {\n" +
+                "        width: 5.625rem;\n" +
+                "        height: 5.625rem;\n" +
+                "        margin: 0;\n" +
+                "    }\n" +
+                "\n" +
+                "    #logo {\n" +
+                "    float: left;\n" +
+                "    margin-top: -6%;\n" +
+                "    }\n" +
+                "\n" +
+                "    #accueil {\n" +
+                "        float: right;\n" +
+                "        padding-top: 1%;\n" +
+                "        padding-right: 1%;\n" +
+                "        margin-top: -6%; \n" +
+                "    }\n" +
+                "\n" +
+                "    .profile-header{height: 150px;}\n" +
+                "}\n" +
+                "@media (max-width: 767px) {\n" +
+                "    .profile-header-img {\n" +
+                "        width: 3.75rem;\n" +
+                "        height: 3.75rem;\n" +
+                "        margin: 0;\n" +
+                "    }\n" +
+                "    .profile-header-info {\n" +
+                "        margin-left: 4.6875rem;\n" +
+                "        padding-bottom: 0.9375rem;\n" +
+                "    }\n" +
+                "    .profile-header-info h4 {\n" +
+                "        margin: 0 0 0.3125rem;\n" +
+                "    }\n" +
+                "    .profile-container {\n" +
+                "        padding: 0.9375rem 0.9375rem 3.6875rem;\n" +
+                "    }\n" +
+                "}\n" +
+                "\n" +
+                ".profile-header-img img {\n" +
+                "    max-width: 100%;\n" +
+                "}\n" +
+                ".table.table-profile th {\n" +
+                "    border: none;\n" +
+                "    color: #000;\n" +
+                "    padding-bottom: 0.3125rem;\n" +
+                "    padding-top: 0;\n" +
+                "}\n" +
+                ".table.table-profile td {\n" +
+                "    border-color: #c8c7cc;\n" +
                 "}" );
 
 
