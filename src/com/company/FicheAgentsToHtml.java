9ff88d8717e2543799457ef44060a3354da9f966
Maxence ROEX <maxence.roex@epsi.fr>
2021-05-12 20:30:37 +0200

    push v2 mspr3

---
+++ src/com/company/FicheAgentsToHtml.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class FicheAgentsToHtml {
+
+    public String render( Agent agents) {
+
+        StringBuilder html = new StringBuilder();
+        html.append( "<!doctype html>\n" );
+        html.append( "<html lang='en'>\n" );
+
+        html.append( "<head>\n" );
+        html.append( "<meta charset='utf-8'>\n" );
+        html.append( "<title>Fiche Agent</title>\n" );
+        html.append( "<link rel=\"stylesheet\" href=\"../stylesheet.css\">" );
+        html.append( "</head>\n\n" );
+        html.append( "<body>\n" );
+        String ID = agents.getPrenom().charAt(0) + agents.getNom();
+        html.append("<h1> "+
+                 agents.getNom()  +" " +  agents.getPrenom() +"\n" +
+                "<img class=\"photo\" src=\"../images/"+ ID.toLowerCase() +".jpg\" />\n" +
+                "\n" +
+                "</h1>\n");
+        html.append("<div id=\"materiel\">\n");
+        html.append("<table id=\"tablemateriel\">\n");
+        html.append("<tr>\n" +
+                "\t    <th width=\"250\"></td>\n" +
+                "\t    <th width=\"50\"></td>\n" +
+                "\t  </tr>\n");
+        for ( String objet : agents.getObjet()) {
+            html.append( "<tr> <td> "+ objet + "</td> <td><img id=\"case\" src=\"../images/image2.png\"/> </td> </tr>\n" );
+        }
+        html.append( "</table>\n" );
+        html.append( "</div>\n" );
+        html.append( "</body>\n\n" );
+        html.append( "</html>" );
+
+        return html.toString();
+    }
+}
