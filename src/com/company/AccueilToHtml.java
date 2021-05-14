05a49f29f0f9e83d587ddf780449b98d890b5d47
cbisso11 <clement.bisson@epsi.fr>
2021-05-14 23:25:01 +0200

    msprJavaV5.

--- src/com/company/AccueilToHtml.java
+++ src/com/company/AccueilToHtml.java
@@ -17,8 +17,8 @@ public String render( String[] IDAgents) {
         html.append("<header>");
         html.append("    <div id=\"header\">");
         html.append("        <h4 id=\"titre\">ACCUEIL</h4>");
-        html.append("        <img src=\"IMAGES/logo.png\" alt=\"\" id=\"logo\" width=\"171\" />");
-        html.append("        <a href=\"#\"><img id=\"accueil\" width=\"80\" src=\"IMAGES/accueil.png\"/></a>");
+        html.append("        <img src=\"../images/logo.png\" alt=\"\" id=\"logo\" width=\"171\" />");
+        html.append("        <a href=\"#\"><img id=\"accueil\" width=\"80\" src=\"../images/accueil.png\"/></a>");
         html.append("    </div>");
         html.append("</header>");
         html.append("<body>");
