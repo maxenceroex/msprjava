4305f3e300d2926d8be1e8fcefe5a50739135a88
cbisso11 <clement.bisson@epsi.fr>
2021-05-17 21:31:30 +0200

    msprJavaV6

--- src/com/company/AccueilToHtml.java
+++ src/com/company/AccueilToHtml.java
@@ -2,69 +2,69 @@ package com.company;
 
 public class AccueilToHtml {
 
-public String render( String[] IDAgents) {
+        public String render( String[] IDAgents) {
 
-        StringBuilder html = new StringBuilder();
-        html.append("<!DOCTYPE html>");
-        html.append("<html lang=\"fr\">");
-        html.append("<head>");
-        html.append("    <meta charset=\"utf-8\">");
-        html.append("    <title>ACCUEIL</title>");
-        html.append("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-        html.append("    <link href=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" rel=\"stylesheet\">");
-        html.append("    <link rel=\"stylesheet\" href=\"stylesheet.css\">");
-        html.append("</head>");
-        html.append("<header>");
-        html.append("    <div id=\"header\">");
-        html.append("        <h4 id=\"titre\">ACCUEIL</h4>");
-        html.append("        <img src=\"../images/logo.png\" alt=\"\" id=\"logo\" width=\"171\" />");
-        html.append("        <a href=\"#\"><img id=\"accueil\" width=\"80\" src=\"../images/accueil.png\"/></a>");
-        html.append("    </div>");
-        html.append("</header>");
-        html.append("<body>");
-        html.append("<div class=\"container\">");
-        html.append("<div id=\"content\" class=\"content p-0\">");
-        html.append("    <div class=\"profile-header\">");
-        html.append("        <div class=\"profile-header-cover\"></div>");
-        html.append("        <div class=\"profile-header-content1\">");
-        html.append("            <div class=\"profile-header-info\">");
-        html.append("                <h4 class=\"m-t-sm\">Liste des agents</h4>");
-        html.append("            </div>");
-        html.append("        </div>");
-        html.append("    </div>");
-        html.append("    <div class=\"profile-container\">");
-        html.append("        <div class=\"row row-space-20\">");
-        html.append("            <div class=\"col-md-8\" id=\"tableau\">");
-        html.append("                <div class=\"tab-content p-0\">");
-        html.append("                    <div class=\"tab-pane active show\" id=\"profile-about\">");
-        html.append("                        <table class=\"table table-profile\">");
-        html.append("                            <thead>");
-        html.append("                                <tr>");
-        html.append("                                    <th colspan=\"2\">Agents</th>");
-        html.append("                                </tr>");
-        html.append("                            </thead>");
-        html.append("                            <tbody>");
-        for ( String idagent : IDAgents) {
-                html.append( "<tr>\n" +
-                        "        <td><a href= \"html/" + idagent + ".html\">"+ idagent + "</a></td>\n" +
-                        "</tr>" );
+                StringBuilder html = new StringBuilder();
+                html.append("<!DOCTYPE html>\n");
+                html.append("<html lang=\"fr\">\n");
+                html.append("<head>\n");
+                html.append("    <meta charset=\"utf-8\">\n");
+                html.append("    <title>ACCUEIL</title>\n");
+                html.append("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
+                html.append("    <link href=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" rel=\"stylesheet\">\n");
+                html.append("    <link rel=\"stylesheet\" href=\"stylesheet.css\">\n");
+                html.append("</head>\n");
+                html.append("<header>\n");
+                html.append("    <div id=\"header\">\n");
+                html.append("        <h4 id=\"titre\">ACCUEIL</h4>\n");
+                html.append("        <img src=\"../images/logo.png\" alt=\"\" id=\"logo\" width=\"171\" />\n");
+                html.append("        <a href=\"#\"><img id=\"accueil\" width=\"80\" src=\"../images/accueil.png\"/></a>\n");
+                html.append("    </div>\n");
+                html.append("</header>\n");
+                html.append("<body>\n");
+                html.append("<div class=\"container\">\n");
+                html.append("<div id=\"content\" class=\"content p-0\">\n");
+                html.append("    <div class=\"profile-header\">\n");
+                html.append("        <div class=\"profile-header-cover\"></div>\n");
+                html.append("        <div class=\"profile-header-content1\">\n");
+                html.append("            <div class=\"profile-header-info\">\n");
+                html.append("                <h4 class=\"m-t-sm\">Liste des agents</h4>\n");
+                html.append("            </div>\n");
+                html.append("        </div>\n");
+                html.append("    </div>\n");
+                html.append("    <div class=\"profile-container\">\n");
+                html.append("        <div class=\"row row-space-20\">\n");
+                html.append("            <div class=\"col-md-8\" id=\"tableau\">\n");
+                html.append("                <div class=\"tab-content p-0\">\n");
+                html.append("                    <div class=\"tab-pane active show\" id=\"profile-about\">\n");
+                html.append("                        <table class=\"table table-profile\">\n");
+                html.append("                            <thead>\n");
+                html.append("                                <tr>\n");
+                html.append("                                    <th colspan=\"2\">Agents</th>\n");
+                html.append("                                </tr>\n");
+                html.append("                            </thead>\n");
+                html.append("                            <tbody>\n");
+                for ( String idagent : IDAgents) {
+                        html.append( "<tr>\n" +
+                                "        <td><a href= \"html/" + idagent + ".html\">"+ idagent + "</a></td>\n" +
+                                "</tr>\n" );
+                }
+                html.append("                            </tbody>\n");
+                html.append("                        </table>\n");
+                html.append("                    </div>\n");
+                html.append("                </div>\n");
+                html.append("            </div>\n");
+                html.append("        </div>\n");
+                html.append("    </div>\n");
+                html.append("</div>\n");
+                html.append("</div>\n");
+                html.append("<script src=\"http://code.jquery.com/jquery-1.10.2.min.js\"></script>\n");
+                html.append("<script src=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\"></script>\n");
+                html.append("<script type=\"text/javascript\">\n");
+                html.append("</script>\n");
+                html.append("</body>\n");
+                html.append("</html>\n");
+
+                return html.toString();
         }
-        html.append("                            </tbody>");
-        html.append("                        </table>");
-        html.append("                    </div>");
-        html.append("                </div>");
-        html.append("            </div>");
-        html.append("        </div>");
-        html.append("    </div>");
-        html.append("</div>");
-        html.append("</div>");
-        html.append("<script src=\"http://code.jquery.com/jquery-1.10.2.min.js\"></script>");
-        html.append("<script src=\"http://netdna.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\"></script>");
-        html.append("<script type=\"text/javascript\">");
-        html.append("</script>");
-        html.append("</body>");
-        html.append("</html>");
-
-        return html.toString();
-    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,29 @@ import java.util.List;
 public class Main {
     public static Agent[] creationAgent() {
         try {
+            //Ouverture de l'accès en lecture au fichier staff.txt
+            //Récupération des ID de chagues agents dans le fichier staff.txt
+            //Dans un tableau de chaine de caractère
             BufferedReader FE = new BufferedReader(new FileReader("infotxt/staff.txt"));
             String ligne = null;
+            //On créer une liste d'agent pour y stocker toutes les instances Agent qu'on crééra par la suite
             List<String> listeIdAgents = new ArrayList<>();
             while ((ligne = FE.readLine()) != null) {
                 listeIdAgents.add(ligne);
             }
             List<Agent> listAgent = new ArrayList<>();
+            //On parcours la liste des ID et pour chacun d'eux on lit le fichier se nommant <ID>.txt
+            //contenue dans le dossier "infotxt"
             for (String idAgent : listeIdAgents) {
+                //Ouverture en lecture du fichier <ID>.txt
                 FE = new BufferedReader(new FileReader("infotxt/"+idAgent + ".txt"));
                 String prenomAgent = null;
                 String nomAgent = null;
                 List<String> objetsAgent = new ArrayList<>();
                 boolean flag = false;
                 int i = 0;
+                // On parcours le fichier <ID>.txt ligne par ligne afin de connaitre les données souhaité de l'agent
+                //qu'on placera dans des variables temporaire
                 while ((ligne = FE.readLine()) != null) {
                     i++;
                     if (i == 1) prenomAgent = ligne;
@@ -33,10 +42,14 @@ public class Main {
                         System.out.println("erreur NullPointerException");
                     }
                 }
+                //On récupère les différentes variables et on instancie notre objet Agents
                 Agent agent = new Agent(prenomAgent, nomAgent, objetsAgent);
+                //On ajoute l'agent nouvellement créé dans la liste d'agent
                 listAgent.add(agent);
             }
+            //On ferme le fichier en lecture
             FE.close();
+            // On retourne la liste de tout les agents créé
             Agent[] myListeAgent = new Agent[listAgent.size()];
             return listAgent.toArray(myListeAgent);
         } catch (FileNotFoundException e) {
